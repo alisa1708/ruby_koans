f2a944ec6a8923f22a94e212a10d3d346b54cafa
alisa1708 <146519346+alisa1708@users.noreply.github.com>
2025-04-26 22:34:34 +0300

    159/295

--- koans/about_triangle_project.rb
+++ koans/about_triangle_project.rb
@@ -1,24 +1,21 @@
-require File.expand_path(File.dirname(__FILE__) + '/neo')
-
-# You need to write the triangle method in the file 'triangle.rb'
-require './triangle'
-
-class AboutTriangleProject < Neo::Koan
-  def test_equilateral_triangles_have_equal_sides
-    assert_equal :equilateral, triangle(2, 2, 2)
-    assert_equal :equilateral, triangle(10, 10, 10)
+# triangle.rb
+def triangle(a, b, c)
+  # Проверка на валидность треугольника
+  sides = [a, b, c].sort
+  unless sides[0] + sides[1] > sides[2]
+    raise TriangleError, "Invalid triangle sides"
   end
 
-  def test_isosceles_triangles_have_exactly_two_sides_equal
-    assert_equal :isosceles, triangle(3, 4, 4)
-    assert_equal :isosceles, triangle(4, 3, 4)
-    assert_equal :isosceles, triangle(4, 4, 3)
-    assert_equal :isosceles, triangle(10, 10, 2)
+  # Определение типа треугольника
+  if a == b && b == c
+    :equilateral
+  elsif a == b || b == c || a == c
+    :isosceles
+  else
+    :scalene
   end
+end
 
-  def test_scalene_triangles_have_no_equal_sides
-    assert_equal :scalene, triangle(3, 4, 5)
-    assert_equal :scalene, triangle(10, 11, 12)
-    assert_equal :scalene, triangle(5, 4, 2)
-  end
+# Класс для обработки ошибок невалидного треугольника
+class TriangleError < StandardError
 end
